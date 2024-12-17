e9ede70debd556ea07162338dd54008d1f76d141
harishkrishna5 <harishkrish09@gmail.com>
2024-12-17 23:47:58 +0530

    Update variable.tf

--- infra/compute/variable.tf
+++ infra/compute/variable.tf
@@ -8,6 +8,7 @@ variable "aws_region" {
 variable "ami_id" {
   description = "The AMI ID to use for the EC2 instance"
   type        = string
+  default = "ami-053b12d3152c0cc71"
 }
 
 # The instance type (e.g., t2.micro, t2.small)
